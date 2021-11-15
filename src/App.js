c648dca210e746cc64c32a7e6715113b8dcb9b43
JasonSchule <apturahimajason@gmail.com>
2021-11-15 12:25:46 +0100

    Revert "git@github.com:crixouss/registerTest.git"
    This reverts commit 9a8a2a256ff90400e0b158cbc6670a4b56864fad.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-import React from "react";
-import Register from "./components/Register";
-import Header from "./components/Header";
-import About from "./components/About";
-import Home from "./components/Home";
+import logo from './logo.svg';
+import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-export default function App () {
+function App() {
   return (
-      <Router>
-        <Header/>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-
-          <hr />
-
-          {
-
-          }
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
